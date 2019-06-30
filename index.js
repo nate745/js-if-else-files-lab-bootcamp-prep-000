7c5e87812f181dc81a9babca947e8bd537be8cc4
Nathan Alex <nate745@gmail.com>
2019-06-30 01:51:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var name = "Susan"
 var height = 70
 
+if (name = "Susan"){
+  console.log("")
+}
+
 if (height = 70){
-  console.log("The height is correct")
-} else {
-  console.log('')
+  console.log("")
 }
 
 
